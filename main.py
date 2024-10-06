abf651b28e8622ab29153e7b1a670c227c55543e
KTRIHUNT <kylehunt555@gmail.com>
2024-10-05 22:11:17 -0700

    Converted json into python list working, json data handling in progress, file cleanup working

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 import requests
 import json
 from argparse import ArgumentParser
+import time
+import os
 
 parser = ArgumentParser()
 parser.add_argument('username')
 
 args = parser.parse_args()
-
+print("Searching for user...")
 
 api = requests.get(f'https://api.github.com/users/{args.username}/events')
 
 if api.status_code == 404:
     print("Invalid User!")
 else:
+    print("User Found! Displaying recent activity...")
+    time.sleep(1)
+
     file = open('user.json', 'w')
     file.write(api.text)
     file.close()
+
+    with open('user.json') as file:
+        file_list = json.load(file)
+        events = {}
+        for event in file_list:
+            try:
+                events[event['type']] += 1
+            except KeyError:
+                events[event['type']] = 1
+        print(events)
+    
+if os.path.exists("user.json"):
+    os.remove("user.json")
